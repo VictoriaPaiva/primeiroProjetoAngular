6e8ae575709196567de4e1940802d20a695403eb
Mayara <victoria.pmoliveira@gmail.com>
2019-12-16 16:34:34 -0300

    teste2 para subir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
+import { CorpoComponent } from './corpo/corpo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    CorpoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
